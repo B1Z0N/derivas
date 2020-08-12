9c5ed16a1f9328672414c4aa56c295104c507136
Mykola Fedurko <mykolaf@compatibl.com>
2020-08-12 16:29:29 +0300

    misc(all): restructured project

---
+++ Derivas/Exception.cs
@@ -0,0 +1,12 @@
+﻿namespace Derivas.Exception
+{
+    /// <summary>
+    /// Base exception class for the project
+    /// </summary>
+    public class DvBaseException : System.Exception
+    {
+        public DvBaseException(string msg) : base(msg)
+        {
+        }
+    }
+}
